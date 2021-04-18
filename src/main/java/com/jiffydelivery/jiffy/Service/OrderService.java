309a3e8f84d54e8f4756ba3401e53a30db247026
SeanXiao705 <71317897+SeanXiao705@users.noreply.github.com>
2021-04-17 20:19:23 -0700

    issue

--- src/main/java/com/jiffydelivery/jiffy/Service/OrderService.java
+++ src/main/java/com/jiffydelivery/jiffy/Service/OrderService.java
@@ -2,12 +2,13 @@ package com.jiffydelivery.jiffy.Service;
 
 import com.jiffydelivery.jiffy.Entity.DBDAO.Order;
 import com.jiffydelivery.jiffy.Repository.OrderRepository;
+import com.jiffydelivery.jiffy.Repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     @Autowired
-    OrderRepository orderRepository;
+    OrderRepositor orderRepository;
 
 }
