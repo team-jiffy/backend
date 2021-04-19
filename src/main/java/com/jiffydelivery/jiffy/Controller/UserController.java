65defeea586693af3017373f23f16595fc254f5b
xren935 <59517115+xren935@users.noreply.github.com>
2021-04-18 19:48:55 -0700

    added UserController

--- src/main/java/com/jiffydelivery/jiffy/Controller/UserController.java
+++ src/main/java/com/jiffydelivery/jiffy/Controller/UserController.java
@@ -1,5 +1,5 @@
 package com.jiffydelivery.jiffy.Controller;
-import com.jiffydelivery.jiffy.Entity.FrontModelEntities.User;
+
 import com.jiffydelivery.jiffy.Entity.Request.CustomerRequest.CustomerCreationRequest;
 
 import com.jiffydelivery.jiffy.Entity.Response.CustomerResponse.CustomerCreationResponse;
@@ -8,48 +8,49 @@ import com.jiffydelivery.jiffy.Entity.Response.CustomerResponse.GetCustomerRespo
 import com.jiffydelivery.jiffy.Entity.Response.CustomerResponse.PasswordUpdateResponse;
 import com.jiffydelivery.jiffy.Entity.Response.CustomerResponse.LogoutResponse;
 
+import com.jiffydelivery.jiffy.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.jiffydelivery.jiffy.Entity.FrontModelEntities.User;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
+    @Autowired UserService userService;
+
     /* maps to API #17. Sign up a user*/
     @PutMapping("/signup")
     public CustomerCreationResponse createOrder(@RequestBody CustomerCreationRequest userRequest) {
-        System.out.println(userRequest.toString());
-        // User newUser = new User(userRequest.email, userRequest.lastName, userRequest.defaultSender, userRequest.UID, userRequest.firstName, userReuqest.phone, userRequest.profilePictureURL, userRequest.defaultRecipient, userRequestpassword, userRequest.location);
-        User newUser = new User("e", "ln", null, "id", "fname", "phone", "url", null, null);
-        return new CustomerCreationResponse("200", "OK", newUser);
+        return userService.CreateCustomerResponse(userRequest);
     }
 
     /* maps to API #18. logout*/
-    @PutMapping("/logout")
-    public LogoutResponse createOrder(@RequestParam(value="UID", required = true, defaultValue = "unknownID") String UID) {
-        System.out.println("UID is: " + UID);
-        return new LogoutResponse("200", "OK");
-    }
+//    @PutMapping("/logout")
+//    public LogoutResponse createOrder(@RequestParam(value="UID", required = true, defaultValue = "unknownID") String UID) {
+//        System.out.println("UID is: " + UID);
+//        return new userService.("200", "OK");
+//    }
 
     /* maps to API #20 update a user's info */
     @PostMapping("/users/updateInfo")
-    public CustomerUpdateResponse updateCustomer(@RequestBody CustomerCreationRequest updateCustomerRequest) {
-        System.out.println(updateCustomerRequest.toString());
+    public CustomerUpdateResponse updateCustomer(@RequestBody User newUser) {
         // TODO: User actual user input to initialize the User
-        User updatedUser = new User("e", "ln", null, "id", "fname", "phone", "url", null, null);
-        return new CustomerUpdateResponse("200", "OK", updatedUser);
+        return userService.updateCustomer(newUser);
     }
 
     /* maps to API #21. get user profile */
     @GetMapping("/user/profile")
     public GetCustomerResponse getCustomer(@RequestParam(value="UID", required = false) String UID) {
-        System.out.println("UID is: " + UID);
-        return new GetCustomerResponse("200", "OK", new User());
+        int uidAsInt=Integer.parseInt(UID);
+        return userService.getCustomer(uidAsInt);
     }
 
     /* maps to API #22. update password */
     @GetMapping("/user/updatePassword")
     public PasswordUpdateResponse updatePassword(@RequestParam(value="UID", required = true, defaultValue="id") String UID, @RequestParam(value="password", required = true, defaultValue="defaultPassword") String password) {
-        System.out.println("UID is: " + UID);
-        return new PasswordUpdateResponse("200", "OK");
+//        System.out.println("UID is: " + UID);
+        return userService.updatePassword(UID, password);
     }
 }
 
